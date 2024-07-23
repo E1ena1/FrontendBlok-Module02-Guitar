198def78748245c2e513eb9ef8298e601e93ad39
ElenaKalin <kalinakrasnaya33@gmail.com>
2024-07-23 23:00:37 +0300

    add part from 16

--- src/index.js
+++ src/index.js
@@ -17,5 +17,20 @@ const swiper = new Swiper('.slider-teacher', {
         prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
-
 });
+// Accordion
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
